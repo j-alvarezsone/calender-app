1ba194f72b9e72ee67e4b657fe3761d29a2d26b8
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-06 15:53:06 +0200

    added react-big-calender

--- src/components/calender/CalenderScreen.jsx
+++ src/components/calender/CalenderScreen.jsx
@@ -1,9 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar } from '../ui/Navbar';
+
+import moment from 'moment';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import { enMessages } from '../../helpers/calender-messages-es';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { CalenderEvent } from './CalenderEvent';
+
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+
+const events = [
+  {
+    title: 'Cumpleaños del jefe',
+    start: moment().toDate(),
+    end: moment().add(2, 'hour').toDate(),
+    bgcolor: '#fafafa',
+    notes: 'Comprar el pastel',
+    user: {
+      _id: '123',
+      name: 'jorge',
+    },
+  },
+];
+
 export const CalenderScreen = () => {
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
+
+  const onDoubleClick = (e) => {
+    console.log(e);
+  };
+
+  const onSelectEvent = (e) => {
+    console.log(e);
+  };
+
+  const onViewChange = (e) => {
+    setLastView(e);
+    localStorage.setItem('lastView', e);
+  };
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block',
+      color: 'white',
+    };
+
+    return {
+      style,
+    };
+  };
+
   return (
-    <div>
+    <div className='calender-screen'>
       <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor='start'
+        endAccessor='end'
+        messages={enMessages}
+        eventPropGetter={eventStyleGetter}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelectEvent}
+        onView={onViewChange}
+        view={lastView}
+        components={{
+          event: CalenderEvent,
+        }}
+      />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 
 import { CalenderApp } from './CalenderApp';
 
+import './styles.css';
+
 ReactDOM.render(<CalenderApp />, document.getElementById('root'));
