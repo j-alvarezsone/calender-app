df3f5639d8d464430cdeb792919b9e6925030742
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-06 13:07:59 +0200

    initial commit

---
+++ src/CalenderApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const CalenderApp = () => {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+};
---
+++ src/components/calender/CalenderScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const CalenderScreen = () => {
+  return (
+    <div>
+      <h1>Calender Screen</h1>
+    </div>
+  );
+};
