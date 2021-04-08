a4136a41fba602f680fef1742ec0b4f0f19103fd
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-08 09:27:39 +0200

    added ract-datetime-picker

--- src/components/calender/CalendarModal.jsx
+++ src/components/calender/CalendarModal.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
+import DateTimePicker from 'react-datetime-picker';
+
 import { customStyles } from '../../helpers/modalCustomStyle';
+import { CustomIcon } from '../ui/CustomIcon';
+import moment from 'moment';
 
 Modal.setAppElement('#root');
+const now = moment().minutes(0).seconds(0).add(1, 'hours');
+const nowPlus1 = now.clone().add(1, 'hours');
 
 export const CalendarModal = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [dateStart, setDateStart] = useState(now.toDate());
+  const [dateEnd, setDateEnd] = useState(nowPlus1.toDate());
 
   const closeModal = () => {
     console.log('Closing...');
-    setIsOpen(false);
+  };
+
+  const handleStartDateChange = (e) => {
+    setDateStart(e);
+  };
+
+  const handleEndDateChange = (e) => {
+    setDateEnd(e);
   };
 
   return (
     <Modal
-      isOpen={isOpen}
+      isOpen={true}
       // onAfterOpen={afterOpenModal}
       onRequestClose={closeModal}
       style={customStyles}
@@ -23,9 +36,57 @@ export const CalendarModal = () => {
       className='modal'
       overlayClassName='modal-fondo'
     >
-      <h1>Hola mundo</h1>
+      <h2> New event </h2>
       <hr />
-      <span>Hola de nuevo....</span>
+      <form className='container'>
+        <div className='form-group'>
+          <label>Start date and time</label>
+          <DateTimePicker
+            onChange={handleStartDateChange}
+            value={dateStart}
+            amPmAriaLabel='Select AM/PM'
+            className='form-control'
+          />
+        </div>
+
+        <div className='form-group'>
+          <label>Date and end time</label>
+          <DateTimePicker
+            onChange={handleEndDateChange}
+            value={dateEnd}
+            minDate={dateStart}
+            amPmAriaLabel='Select AM/PM'
+            className='form-control'
+          />
+        </div>
+
+        <hr />
+        <div className='form-group'>
+          <label>Title and notes</label>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='Event title'
+            name='title'
+            autoComplete='off'
+          />
+          <small id='emailHelp' className='form-text text-muted'>
+            A short description
+          </small>
+        </div>
+
+        <div className='form-group'>
+          <textarea type='text' className='form-control' placeholder='Notes' rows='3' name='notes'></textarea>
+          <small id='emailHelp' className='form-text text-muted'>
+            Additional Information
+          </small>
+        </div>
+
+        <button type='submit' className='btn btn-outline-primary btn-block'>
+          <CustomIcon className={'far fa-save'} />
+          <span> Save</span>
+        </button>
+      </form>
     </Modal>
   );
 };
