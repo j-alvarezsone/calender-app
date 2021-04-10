b4aa795237260fb5e147df33b67a955dc62e74b7
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-10 13:53:23 +0200

    added redux

--- src/CalenderApp.jsx
+++ src/CalenderApp.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store/store';
 import { AppRouter } from './router/AppRouter';
 
 export const CalenderApp = () => {
   return (
-    <div>
+    <Provider store={store}>
       <AppRouter />
-    </div>
+    </Provider>
   );
 };
