05eaf5aa348f222b97288e72102b6cbd452c636d
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-12 00:33:10 +0200

    added calender events actions

--- src/redux/types/types.js
+++ src/redux/types/types.js
@@ -1,4 +1,8 @@
 export const types = {
   uiOpenModal: '[ui] Open modal',
   uiCloseModal: '[ui] Close modal',
+
+  // calender
+  eventSetActive: '[event] Set active',
+  eventAddNew: '[event] Add new',
 };
