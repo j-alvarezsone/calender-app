67c3c017c5ccccb667904cec04b84ef228d919fe
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-12 00:35:39 +0200

    added calenderReducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
-import { uiReducer } from './uiReducer';
+import { calenderReducer } from './calender/calenderReducer';
+import { uiReducer } from './ui/uiReducer';
 
 export const rootReducer = combineReducers({
   ui: uiReducer,
+  calender: calenderReducer,
 });
