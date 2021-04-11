5d10268edd73d41d9d211ee546465a22aec24bb1
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-12 00:34:13 +0200

    added calenderReducer

---
+++ src/redux/reducers/calender/calenderReducer.js
@@ -0,0 +1,39 @@
+import moment from 'moment';
+import { types } from '../../types/types';
+
+const INITIAL_STATE = {
+  events: [
+    {
+      id: new Date().getTime(),
+      title: 'Cumpleaños del jefe',
+      start: moment().toDate(),
+      end: moment().add(2, 'hour').toDate(),
+      bgcolor: '#fafafa',
+      notes: 'Comprar el pastel',
+      user: {
+        _id: '123',
+        name: 'jorge',
+      },
+    },
+  ],
+  activeEvent: null,
+};
+
+export const calenderReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case types.eventSetActive:
+      return {
+        ...state,
+        activeEvent: action.payload,
+      };
+
+    case types.eventAddNew:
+      return {
+        ...state,
+        events: [...state.events, action.payload],
+      };
+
+    default:
+      return state;
+  }
+};
