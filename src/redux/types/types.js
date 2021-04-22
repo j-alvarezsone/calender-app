9d2c2803eb62033343d4b8e7d9628980467c9dad
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-22 14:12:05 +0200

    added auth types

--- src/redux/types/types.js
+++ src/redux/types/types.js
@@ -8,4 +8,13 @@ export const types = {
   eventClearActive: '[event] Clear active event',
   eventUpdated: '[event] Updated event',
   eventDeleted: '[event] Delete event',
+
+  // auth
+  authChecking: '[auth] Checking login state',
+  authCheckingFinish: '[auth] Finish checking login state',
+  authStartLogin: '[auth] Start login',
+  authLogin: '[auth] login',
+  authStartRegister: '[auth] Start register',
+  authStartTokenRenew: '[auth] Start token renew',
+  authLogout: '[auth] Logout',
 };
