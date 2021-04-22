18354e9346e10e57b6a8828298a502eeead5888c
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-22 14:12:35 +0200

    added authReducer

---
+++ src/redux/reducers/auth/authReducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  checking: true,
+  // uid: null,
+  // name: null,
+};
+
+export const authReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
