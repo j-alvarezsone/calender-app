5237a27dc4f205c25c2944fe975d6ca12d717afb
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-23 11:40:56 +0200

    added startChecking with useReducer

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalenderScreen } from '../components/calender/CalenderScreen';
+import { startChecking } from '../redux/actions/auth/auth';
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(startChecking());
+  }, [dispatch]);
+
   return (
     <Router>
       <div className='main'>
