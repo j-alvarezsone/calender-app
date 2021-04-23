fa428325c1e62981d72b5d79e77bd8a47893f59e
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-23 10:25:33 +0200

    added authLogin

--- src/redux/reducers/auth/authReducer.js
+++ src/redux/reducers/auth/authReducer.js
@@ -1,3 +1,5 @@
+import { types } from '../../types/types';
+
 const INITIAL_STATE = {
   checking: true,
   // uid: null,
@@ -6,6 +8,12 @@ const INITIAL_STATE = {
 
 export const authReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case types.authLogin:
+      return {
+        ...state,
+        checking: false,
+        ...action.payload,
+      };
     default:
       return state;
   }
